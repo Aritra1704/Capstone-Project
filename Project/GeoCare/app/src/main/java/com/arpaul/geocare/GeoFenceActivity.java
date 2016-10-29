d764d14d1346031fb6899e5db7a87bdfda292f62
Aritra1704 <aritra1704@gmail.com>
2016-10-29 16:56:42 +0530

    Added Map resources and initial screen.

---
+++ Project/GeoCare/app/src/main/java/com/arpaul/geocare/GeoFenceActivity.java
@@ -0,0 +1,13 @@
+package com.arpaul.geocare;
+
+/**
+ * Created by ARPaul on 29-10-2016.
+ */
+
+public class GeoFenceActivity extends BaseActivity {
+
+    @Override
+    public void initialize() {
+
+    }
+}
---
+++ Project/GeoCare/app/src/main/java/com/arpaul/geocare/LocationSearchActivity.java
@@ -0,0 +1,13 @@
+package com.arpaul.geocare;
+
+/**
+ * Created by ARPaul on 29-10-2016.
+ */
+
+public class LocationSearchActivity extends BaseActivity {
+
+    @Override
+    public void initialize() {
+
+    }
+}
