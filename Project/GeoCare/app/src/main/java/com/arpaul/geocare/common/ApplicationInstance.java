c6fbfc5c352e368eff90488509752f37a97fedfe
Aritra1704 <aritra1704@gmail.com>
2016-10-29 16:23:09 +0530

    Added Base in geocare.

---
+++ Project/GeoCare/app/src/main/java/com/arpaul/geocare/common/ApplicationInstance.java
@@ -0,0 +1,14 @@
+package com.arpaul.geocare.common;
+
+/**
+ * Created by Aritra on 19-09-2016.
+ */
+public class ApplicationInstance {
+
+    public static final int LOADER_FETCH_ADDRESS        = 1;
+    public static final int LOADER_FETCH_LOCATION       = 2;
+    public static final int LOADER_SAVE_LOCATION        = 3;
+    public static final int LOADER_FETCH_ALL_LOCATION   = 4;
+
+    public static final String LOCK_APP_DB              = "LOCK_APP_DB";
+}
